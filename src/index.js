5410b7c9cf64fa3dbd3e9b241a92f217ddffd5bd
Atsighi Bright <brytebee@gmail.com>
2021-11-09 15:53:59 +0100

    Update page text

--- src/index.js
+++ src/index.js
@@ -1,6 +1,45 @@
 import './style.css';
+const dommyList = [
+  {
+    userName: 'Matthew',
+    userScore: 33
+  },
+  {
+    userName: 'Jane',
+    userScore: 38
+  },
+  {
+    userName: 'Banny',
+    userScore: 28
+  },
+  {
+    userName: 'Judas',
+    userScore: 20
+  },
+  {
+    userName: 'Larry',
+    userScore: 27
+  },
+  {
+    userName: 'Haji',
+    userScore: 39
+  },
+];
 
 const root = document.querySelector('#root');
-const h1 = document.createElement('h1');
-h1.textContent = 'Hello World from JS';
-root.append(h1);
+
+dommyList.forEach((item) => {
+  const li = document.createElement('li');
+  const userName = document.createElement('span');
+  const userScore = document.createElement('span');
+
+  li.classList = 'single-list-item';
+  userName.classList = 'user-name';
+  userScore.classList = 'user-score';
+
+  userName.textContent = item.userName;
+  userScore.textContent = item.userScore;
+
+  li.append(userName, userScore);
+  root.appendChild(li);
+});
