9a2b144740ce4cabec2f1ccb34bb42addaba5d32
Atsighi Bright <brytebee@gmail.com>
2021-11-10 15:13:00 +0100

    Fix linters

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
-    publicPath: "/LeaderBoard_/",
+    publicPath: '/LeaderBoard_/',
   },
   devServer: {
     static: './dist',
