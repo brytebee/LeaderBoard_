45f78b8d91e35734540032dc21f7577df29112cf
Atsighi Bright <brytebee@gmail.com>
2021-11-21 00:42:13 +0100

    Fix linters

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import { getScores, postScore } from './modules/api.js';
 
 document.querySelector('#submit').addEventListener('click', (e) => {
   if (
-    document.querySelector('#name').value !== '' &&
-    document.querySelector('#score').value !== ''
+    document.querySelector('#name').value !== ''
+    && document.querySelector('#score').value !== ''
   ) {
     e.preventDefault();
     postScore().then(() => getScores());
